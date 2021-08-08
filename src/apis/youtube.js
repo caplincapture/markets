488a81c3d26da7f24b6d41a7b1211f00c3954d37
caplincapture <leuping@gmail.com>
2021-08-08 13:42:37 -0700

    deploy

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const KEY = "AIzaSyAq4sV_Xogvvm_VGv2Lg7WcGqGjW4F79og";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
